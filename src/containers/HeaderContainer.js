e1b005f7a75bdcfa0ae095bb5b09f53f22de0478
Satyanarayana Devi <connect2satyam@gmail.com>
2022-03-11 22:48:11 +0530

    small

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -29,7 +29,7 @@ const HeaderContainer = ({ bg, source }) => {
         <HeaderWrapper.Container>
           <HeaderWrapper.Title bg={bg}>
             <HeaderWrapper.Link bg={bg} fixed={fixed} to="/">
-              Real Home
+              Lakshmi Landmark Pvt Ltd
             </HeaderWrapper.Link>
           </HeaderWrapper.Title>
           <HeaderWrapper.LinksContainer>
